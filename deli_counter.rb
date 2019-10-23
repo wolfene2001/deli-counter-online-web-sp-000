548f838fab2c13f75a08357545859702515012b7
wolfene2001 <wolfene2001@yahoo.com>
2019-10-23 19:43:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,6 @@ def now_serving(katz_deli)
   if katz_deli == 0
   puts "There is nobody waiting to be served!"
 elsif katz_deli.length > 0
-  return "Currently serving " + katz_deli.shift ""
+  return "Currently serving " + katz_deli.shift
 end
 end  
