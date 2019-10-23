384102070d975a1ca9fda0f56e7259ab2b37dc2a
wolfene2001 <wolfene2001@yahoo.com>
2019-10-23 19:29:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 katz_deli = []
 def line(katz_deli)
   if katz_deli = 0
-  puts "The line is currently empty."  
+  puts "The line is currently empty."
 elsif katz_deli.length > 0
   katz_deli.each_with_index do
   katz_deli <<"The line is currently: katz_deli.inspect"
