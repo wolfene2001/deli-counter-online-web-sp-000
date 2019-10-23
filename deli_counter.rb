1a4ee230c5f6a200ed8410ad43ca7dfb4f468793
wolfene2001 <wolfene2001@yahoo.com>
2019-10-23 19:43:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,11 @@ def take_a_number(katz_deli, name)
   return "Welcome, #{name}. You are number {katz_deli.length} in line."
 
 end
+
+def now_serving(katz_deli)
+  if katz_deli == 0
+  puts "There is nobody waiting to be served!"
+elsif katz_deli.length > 0
+  return "Currently serving " + katz_deli.shift ""
+end
+end  
