d4986ec0151add159d330b233ce0c80750e7e975
wolfene2001 <wolfene2001@yahoo.com>
2019-10-23 19:30:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
-  if katz_deli = 0
+  if katz_deli == 0
   puts "The line is currently empty."
 elsif katz_deli.length > 0
   katz_deli.each_with_index do
