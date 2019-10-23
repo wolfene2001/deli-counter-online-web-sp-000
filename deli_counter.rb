2d7af2d06f786efb39c2807eea976c94a41dbc5b
wolfene2001 <wolfene2001@yahoo.com>
2019-10-23 20:07:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  return "Welcome, #{name}. You are number {katz_deli.length} in line."
+  return "Welcome, #{name}. You are number #{katz_deli.length} in line."
 
 end
 
